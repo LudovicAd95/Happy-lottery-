561ecd4274f49e6fd8269d2ff6e37059672dbf66
Maxence Choiselle <mchoiselle@gmail.com>
2024-12-03 16:29:53 +0100

    [feat] homepage in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import Homepage from './components/HomePage'
-import './App.css'
+import { useState } from 'react';
+import Homepage from './components/HomePage';
+import LotteryPage from './components/lotteryPage';
+import './App.css';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
 
+  const handleNavigate = (page: string) => {
+    setCurrentPage(page);
+  };
 
   return (
     <>
-    <Homepage />
-
-    <p>page principale</p>
+      {currentPage === 'home' && <Homepage onNavigate={handleNavigate} />}
+      {currentPage === 'lottery' && <LotteryPage />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/lotteryPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LotteryPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Page de la loterie</h1>
+      <p>Contenu de la page de la loterie</p>
+    </div>
+  );
+}
+
+export default LotteryPage;

1e237d3e75a8e590fb9d89b6d7b9540352eeab0f
Maxence Choiselle <mchoiselle@gmail.com>
2024-12-03 14:59:18 +0100

    [Feat] Homepage Start

---
+++ src/components/homePage.tsx
@@ -0,0 +1,23 @@
+import '../styles/homePage.css'
+
+function homepage () {
+  return (
+    <>
+    <div className="rules">
+      <h1>Les règles :</h1>
+      <p>Chaque enfant qui le souhaite à le droit de participer une fois à notre loterie.</p>
+      <p>Nous devons renseigner son nom et prénom , son genre , ainsi que son âge. </p>
+      <p>Nous tirons ensuite au sort le cadeau qui lui sera attribué</p>
+      <p>Tout enfant participant à la loterie doit être recensé dans un tableau</p>
+      <p>Il y a 20 lots à gagner</p>
+
+      </div>
+<div>
+
+  <button>Participer</button>
+</div>
+</>
+  );
+}
+
+export default homepage;

493f8d57a320d95bf75da9a7115722e227a9e7a9
Maxence Choiselle <mchoiselle@gmail.com>
2024-12-03 16:40:50 +0100

    [feat] Homepage finished

--- src/components/lotteryPage.tsx
+++ src/components/lotteryPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LotteryPage: React.FC = () => {
   return (
-    <div>
+    <div >
       <h1>Page de la loterie</h1>
       <p>Contenu de la page de la loterie</p>
     </div>
